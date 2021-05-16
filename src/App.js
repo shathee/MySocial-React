44d0bb8143591088f3efc17068bf4988b53fed77
Baki <sat.sust@gmail.com>
2021-05-17 00:44:28 +0200

    material UI Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container >
+       <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <AppBar position="static">
+            <Typography variant="h3" noWrap>
+              MySocial-React
+            </Typography>
+          </AppBar>
+        </Grid>
+        <Grid item xs={16} sm={8}>
+          <Paper >xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={8} sm={4}>
+          <Paper >xs=12 sm=6</Paper>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
