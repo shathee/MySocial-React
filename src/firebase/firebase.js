ebe5669bf5843c9967e33d21b9c82ac1ae554aa4
Baki <sat.sust@gmail.com>
2021-05-17 02:58:34 +0200

    firebase config added

---
+++ src/firebase/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBpEmGRoBvZ_BqVS-RoRnfbaBrQZBPlT5g",
+    authDomain: "mysocial-f0589.firebaseapp.com",
+    projectId: "mysocial-f0589",
+    storageBucket: "mysocial-f0589.appspot.com",
+    messagingSenderId: "939125916697",
+    appId: "1:939125916697:web:87b95dce5373886f3ed688",
+    measurementId: "G-JC4EMGMF94"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAnalytics = firebase.analytics();
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+export {db, auth, provider, storage}
